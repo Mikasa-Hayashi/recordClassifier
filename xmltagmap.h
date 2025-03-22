501ac704967f800e15eea865c4260533c9a12e17
Ivan <ivandenisovich2005@gmail.com>
2025-03-22 11:22:15 +0300

    Added TagInfo struct

--- xmltagmap.h
+++ xmltagmap.h
@@ -1,5 +1,22 @@
+#include <QObject>
+
 #ifndef XMLTAGMAP_H
 #define XMLTAGMAP_H
+
+
+struct TagInfo
+{
+    QString name;
+    QList<QString> attributes;
+    bool hasChildren;
+    QList<QString> children;
+
+    TagInfo(const QString & n, const QList<QString> & attrs, bool hasKids, const QList<QString> & kids = {})
+        : name(n), attributes(attrs), hasChildren(hasKids), children(kids)
+    {
+
+    }
+};
 
 
 class XMLTagMap
